c0fa73e9948aa8c00eaa8bf47a3c877bcee8f3a0
Paula <paularamirezsot@gmail.com>
2021-07-19 18:41:03 -0400

    Templates conectados

--- src/lib/view/templateRegister.js
+++ src/lib/view/templateRegister.js
@@ -2,10 +2,46 @@ export const register = () => {
     const saludo = document.createElement('div');
 
     const viewSaludo =  `
-    <ul>
-      <a href="#/posting"><input type="button"></a>
-      <p>Hola mundo soy el registro</p>
-    </ul>`;
+
+  <form class="form">
+    
+    <h1>Ingresa Tus Datos</h1>
+  <div class="contenedor-derecho"> 
+
+    <div class="input-container">
+      <i></i>
+      <input type="text" id="name"  placeholder="Nombre Completo">
+    </div>
+
+    <div class="input-container">
+      <i></i>
+      <input type="text"  id="gmail"  placeholder="correo@example.com">
+    </div>
+
+    <div class="input-container">
+      <i></i>
+      <input type="password"  id="contraseña"  placeholder="Contraseña">
+    </div> 
+    <div>
+    <input type="button"  class="btn" value="Registrarse" href="#/posting">
+  </div>
+  <button class="iniciarConGoogle" id="iniciarConGoogle">iniciar sesion con google</button>
+
+    
+
+
+</div>
+</form>
+
+
+    
+
+
+     
+
+    `;
+    
+
 
    saludo.innerHTML = viewSaludo;
     return saludo;
