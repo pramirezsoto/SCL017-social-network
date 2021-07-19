fc71df1bf41036b3877aa2327c1f1cef071f63d3
Nicole Quijada <quijadabnicolea@gmail.com>
2021-07-18 20:40:18 -0400

    creando funcion de login con firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
+import {firebaseConfig} from './lib/config/configFirebase.js';
+import {createUserWithPassword, signInWithPassword} from './lib/auth/authetication.js';
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+// createUserWithPassword("quijadabnicolea@gmail.com", "123456", "nicole");
+signInWithPassword("quijadabnicolea@gmail.com", "123456");
+
 
 myFunction();
