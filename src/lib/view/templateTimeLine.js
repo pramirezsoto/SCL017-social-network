56b45d014702067b9fa58191f0ff0fbb8c787bc3
Nicole Quijada <quijadabnicolea@gmail.com>
2021-07-19 18:52:27 -0400

    estructura time line

--- src/lib/view/templateTimeLine.js
+++ src/lib/view/templateTimeLine.js
@@ -4,22 +4,19 @@ export const timeLine = () => {
     const viewPosting =  `
     <div class="time-line">
       <div class="banner-left">
-        <a href="" class="icon-users">Amigos</a>
+        <a href="" class="icon-home">Inicio</a>
         <a href="" class="icon-mobile">FAQ</a> 
         <a href="" class="icon-lock">Politicas de privacidad</a>
         <a href=""class="icon-pencil">Normativas</a>
       </div>
       <div class="banner-right">
-        <a href="" class="icon-users">Amigos</a>
-        <a href="" class="icon-mobile">FAQ</a> 
-        <a href="" class="icon-lock">Politicas de privacidad</a>
-        <a href=""class="icon-pencil">Normativas</a>
+        <h3> Intereses </h3>
+        <a href="">Tips de viaje</a>
+        <a href="">Destinos turisticos</a> 
+        <a href="">Trekking</a>
+        <a href="">Senderismo</a>
       </div>
-    </div>
-    <ul>
-      <a href="#/posting"><input type="button"></a>
-      <p>Hola mundo soy la pagina</p>
-    </ul>`;
+    </div>`;
 
    posting.innerHTML = viewPosting;
     return posting;
