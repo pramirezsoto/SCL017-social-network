4de363ad14bf760cf85b05eab640e1989c79611a
Nicole Quijada <quijadabnicolea@gmail.com>
2021-07-26 08:29:16 -0400

    add classList pantallas

--- src/lib/router.js
+++ src/lib/router.js
@@ -9,19 +9,22 @@ export const changeRoute = (hash) => {
 
 const showTemplate = (hash) => {
     const containerRoot = document.getElementById('root')
+    const footer = document.getElementById('footer');
     switch (hash) {
         //asignamos un caso distinto para cada template
         case "" : 
         case '#/login':
-            containerRoot.style.height = '75vh'
-            containerRoot.style.display = 'flex'
-            containerRoot.style.justifyContent = 'space-around'
+            containerRoot.classList.add('login');
             containerRoot.innerHTML = login().innerHTML;
             break;
         case '#/register':
+            
             containerRoot.innerHTML = register().innerHTML;
             break;
         case '#/posting':
+            containerRoot.classList.remove('login');
+            footer.classList.add('hide');
+            containerRoot.classList.add('posting');
             containerRoot.innerHTML = timeLine().innerHTML;
             break;
         
--- src/lib/view/templateTimeLine.js
+++ src/lib/view/templateTimeLine.js
@@ -4,19 +4,22 @@ export const timeLine = () => {
     const viewPosting =  `
     <div class="time-line">
       <div class="banner-left">
-        <a href="" class="icon-home">Inicio</a>
-        <a href="" class="icon-mobile">FAQ</a> 
-        <a href="" class="icon-lock">Politicas de privacidad</a>
-        <a href=""class="icon-pencil">Normativas</a>
-      </div>
-      <div class="banner-right">
-        <h3> Intereses </h3>
-        <a href="">Tips de viaje</a>
-        <a href="">Destinos turisticos</a> 
-        <a href="">Trekking</a>
-        <a href="">Senderismo</a>
-      </div>
-    </div>`;
+        <a class="icon-home" href="">Inicio</a>
+        <a class="icon-newspaper" href="">Muro</a>
+        <a class="icon-user" href="">Perfil</a>
+        <a class="icon-exit" href="">Cerra sesión</a>
+
+       </div>
+    <div class="banner-right">
+      <h2>Intereses</h2>
+      <a href="">Tips de viaje</a>
+      <a href="">Destinos turisticos</a>
+      <a href="">Trekking</a>
+      <a href="">Senderismo</a>
+  
+    </div>
+  </div>
+    `;
 
    posting.innerHTML = viewPosting;
     return posting;
