30906aea9d0789ae87fb77bb68c89765012afbda
Paula <paularamirezsot@gmail.com>
2021-08-05 09:56:58 -0400

    cambio color prueba

--- src/lib/router.js
+++ src/lib/router.js
@@ -4,7 +4,7 @@ import { timeLine } from './view/templateTimeLine.js'
 import { footer } from './view/templateFooter.js'
 import { firestoreRead, firestoreSave, firestoreDelete, firestoreLike } from './database/firestore.js'
 import { createUserWithPassword, signInWithPassword, signInWithGoogle, currentUser } from './auth/authetication.js';
-//import {eliminar} from './database/firestore.js'//
+
 
 //cambiar la url para que no se vea el gatito
 export const changeRoute = (hash) => {
@@ -128,7 +128,7 @@ export const showTemplate = async(hash) => {
             window.history.replaceState({}, 'posting', '/posting');
             showTemplate('/posting');
     }
-
+    //Eliminar post//
     if (hash.startsWith('/deletePost')) {
       const docId = hash.replace('/deletePost/', '');
       await firestoreDelete(docId);
