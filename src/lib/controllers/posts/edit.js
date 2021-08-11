25e7a9bf7bc8771cdff93600f38a1602e0e0ae17
Paula <paularamirezsot@gmail.com>
2021-08-11 13:29:10 -0400

    id de editar

--- src/lib/controllers/posts/edit.js
+++ src/lib/controllers/posts/edit.js
@@ -4,16 +4,27 @@ export const editPostInput = (e) => {
     const posting = e.srcElement.closest('.post').querySelector(".content").innerHTML;
     const post = document.getElementById('post');
     post.value = posting;
-}
+    console.log("aqui pasa esto", e.srcElement.closest('.post').querySelector(".post-head").id);
 
-export const editPost = async (currentUser) => {
+
+    const postId = document.getElementById('shared2').addEventListener("click" , e.srcElement.closest('.post').querySelector(".post-head").id , 
+    firebase.auth().onAuthStateChanged(user => {
+      console.log( "este es el useruid" , user.uid);
+      editPost(user.uid , postId);
+
+      }))
+    }
+
+
+
+export const editPost = async (currentUserUid , docId) => {
   const content = document.getElementById('post').value;
-  const docId = 'tienes que buscar el id del post';
+  
 
   const postAEditar = {
     content: content,
-    useruid: currentUser.uid
+    useruid: currentUserUid
   };
-
+console.log(docId , postAEditar)
   await firestoreEdit(docId, postAEditar);
 }
